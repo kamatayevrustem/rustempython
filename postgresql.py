ee26b1bfc8ad7226cc20451d606eab905aad1029
kamatayevrustem <61201673+kamatayevrustem@users.noreply.github.com>
2020-07-29 22:44:48 +0600

    изменил по комментариям

--- postgresql.py
+++ postgresql.py
@@ -1,91 +1,103 @@
 import psycopg2
 
+conn = psycopg2.connect(database='rus223445101345', user='rus223445101345', password='rus223445101345',
+                        host='pg.codecontrol.ru', port=59432)
+cur = conn.cursor()
 
-def get_connection():
-    conn = psycopg2.connect(database='', user='', password='', host='pg.codecontrol.ru', port=59432)
-    conn.autocommit = True
-    cursor = conn.cursor()
-    return conn, cursor
+def main():
+    drop = """
+    """
 
+    conn = psycopg2.connect(database='rus223445101345', user='rus223445101345', password='rus223445101345', host='pg.codecontrol.ru', port=59432)
+    cur = conn.cursor()
+    cur.execute(""" DROP TABLE IF EXISTS students CASCADE;
+                DROP TABLE IF EXISTS courses CASCADE;""")
+    conn.commit()
+    create_db()
 
-def close_connection():
-    cursor.close()
+    students_1 = [
+    {'name': 'Аида', 'gpa': 3, 'birth': '1981-07-07'},
+    {'name': 'Аня', 'gpa': 4, 'birth': '1981-07-07'},
+    {'name': 'Анеля', 'gpa': 5, 'birth': '1981-07-07'}
+    ]
+    students_2 = [
+    {'name': 'Алина', 'gpa': 4, 'birth': '1981-07-07'},
+    {'name': 'Алиса', 'gpa': 4, 'birth': '1981-07-07'},
+    {'name': 'Малика', 'gpa': 5, 'birth': '1981-07-07'},
+    ]
+    students_3 = [
+    {'name': 'Малина', 'gpa': 4, 'birth': '1981-07-07'},
+    {'name': 'Света', 'gpa': 3, 'birth': '1981-07-07'},
+    {'name': 'Рябина', 'gpa': 4, 'birth': '1981-07-07'},    ]
+    courses = ['Программирование на Python', 'Django']
+
+    for course in courses:
+        add_course(course)
+    for student in students_1:
+        add_student(student)
+
+    add_students(1, students_2)
+    add_students(2, students_3)
+
+    print(get_student(3))
+    print(get_students(2))
+
+    cur.close()
     conn.close()
 
-
+# создает таблицы БД
 def create_db():
-    global conn
-    global cursor
-    conn, cursor = get_connection()
-    cursor.execute('select version()')
-    sql = """
-        CREATE TABLE IF NOT EXISTS COURSES (
+    tables = """CREATE TABLE courses (
             id SERIAL PRIMARY KEY,
-            name char(100) NOT NULL
-        );
-        CREATE TABLE IF NOT EXISTS STUDENTS (
-            id SERIAL,
-            course_id INTEGER REFERENCES COURSES(id),
-            name char(100) NOT NULL,
+            name VARCHAR(100) NOT NULL);
+            CREATE TABLE students (
+            id SERIAL PRIMARY KEY,
+            course_id INTEGER REFERENCES courses(id),
+            name VARCHAR(100) NOT NULL,
             gpa numeric(10,2),
-            birth date,
-            PRIMARY KEY (id, course_id)
-        );
-    """
-    cursor.execute(sql)
+            birth VARCHAR(100) NULL);"""
 
+    cur.execute(tables)
+    conn.commit()
 
-def add_course(course_name):
-    sql = 'insert into courses (name) values (%s)'
-    cursor.execute(sql, (course_name, ))
+# добавление курса
+def add_course(course):
+    request = """
+    insert into courses (name)
+    values (%s)"""
+    cur.execute(request, [course])
+    conn.commit()
 
-
-def add_students(course_id, students_data_list):
-    for student_data in students_data_list:
-        add_student(course_id, student_data)
-
-
-def add_student(course_id, student_dict):
-    name = student_dict.get('name')
-    gpa = student_dict.get('gpa')
-    birth = student_dict.get('birth')
-    sql = '''
-    insert into students (course_id, name, gpa, birth)
-    values (%s, %s, %s, %s)
-    '''
-    cursor.execute(sql, [course_id, name, gpa, birth])
-
-
-def get_courses():
-    sql = 'select * from courses'
-    cursor.execute(sql)
-    return cursor.fetchall()
-
-
+# вывод списка студентов на курсе
 def get_students(course_id):
-    cursor.execute('select * from students where course_id = %s', [course_id])
-    return cursor.fetchall()
+    cur.execute('select * from students where course_id=%s', [course_id])
+    return cur.fetchall()
 
 
+# запись созданных студентов на курс
+def add_students(course_id, students):
+    for student in students:        #
+        name, gpa, birth = student.values()
+        request = """
+        insert into students (course_id, name, gpa, birth)
+        values (%s, %s, %s, %s)"""
+        cur.execute(request, (course_id, name, gpa, birth))
+        conn.commit()
+
+# создание студентов в таблице БД
+def add_student(student):
+    name, gpa, birth = student.values()
+    request = """
+    insert into students (name, gpa, birth)
+    values (%s, %s, %s)"""
+    cur.execute(request, (name, gpa, birth))
+    conn.commit()
+
+# получение студентов из таблицы БД
 def get_student(student_id):
-    cursor.execute('select * from students where id=%s', [student_id])
-    return cursor.fetchall()
+    cur.execute('select * from students where id=%s', [student_id])
+    return cur.fetchall()
 
 
 if __name__ == '__main__':
-
-    students_python = [
-        {'name': 'Рустем', 'gpa': 4, 'birth': '1981-07-07'},
-        {'name': 'Нариман', 'gpa': 3.5, 'birth': '1982-07-07'},
-        {'name': 'Арсен', 'gpa': 3.6, 'birth': '1983-07-07'},
-    ]
-
-    create_db()
-
-    add_course('Python')
-    print(get_courses())
-
-    add_students(1, students_python)
-    print(get_students(1))
-
-    close_connection()
+    main()
